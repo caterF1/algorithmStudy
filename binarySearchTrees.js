448f2190f6c9a9b1150fc823deb5b111073bfe05
sam <samantha.liu@publicgoodprojects.org>
2020-04-26 11:23:14 -0700

    Traverse a tree

--- binarySearchTrees.js
+++ binarySearchTrees.js
@@ -66,4 +66,59 @@ class BinarySearchTrees {
         }
         return false;
     }
+    //there is error in mu method
+    BFSMyVersion() {
+        let queue = [], visited = [];
+        if (!this.root) return false;
+        queue.push(this.root);
+        while (queue) {
+            let temp = queue.shift();
+            visited.push(temp);
+            if (temp.left) queue.push(temp.left);
+            if (temp.right) queue.push(temp.right)
+        }
+        return visited;
+    }
+    BFS() {
+        let data = [], queue = [], node = this.root;
+        queue.push(node);
+        while (queue.length) {
+            node = queue.shift();
+            data.push(node.value);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        return data;
+    }
+    DFSPreOrder() {
+        let data = [], current = this.root;
+        function traverse(node) {
+            data.push(node.value);
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+        }
+        traverse(current);
+        return data;
+    }
+    DFSPostOrder() {
+        let data = [], current = this.root;
+        function traverse(node) {
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+            data.push(node.value);
+        }
+        traverse(current);
+        return data;
+    }
+    DFSInOrder() {
+        let data = [], current = this.root;
+        function traverse(node) {
+            if(node.left) traverse(node.left);
+            data.push(node.value);
+            if(node.right) traverse(node.right);
+        }
+        traverse(current);
+        return data;
+    }
+
 }
